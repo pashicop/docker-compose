3301096a184bd3034d6fa3893901bfc6dd9c2bfa
pashi <pashicop@gmail.com>
2022-04-25 01:08:50 +0300

    init

---
+++ docker-compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g0obadh057s47imrpv"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gp4seucrmq379e3ogd"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8fehaetisd0nh4r2lr"
+}
